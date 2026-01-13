a7ecc953cc25cc291010b03b34e29cba7527149a
anthonyzhu1024 <anthony.zhu1024@gmail.com>
2026-01-13 10:41:28 +0000

    Deploying to gh-pages from @ anthonyzhu1024/nav@faf1c2b9285645a1fc9f5c7cbbf90ac5eac65fec 🚀

---
+++ chunk-CIG6MR2O.js
@@ -0,0 +1,1 @@
+import{d as a}from"./chunk-OBY64E3N.js";import{Ab as e,sb as r}from"./chunk-ONDZUQL3.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
